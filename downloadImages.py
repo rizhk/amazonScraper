93698da7eb5a3e1fd47cf37217bbe3b3115434fe
Rizwan Mushtaq <magento.guru@yahoo.com>
2023-06-14 16:44:08 +0800

    adding spacy

--- downloadImages.py
+++ downloadImages.py
@@ -1,9 +1,9 @@
 # Description : Download images from pexels.com and save them into a folder
 import requests
 from pexels_api import API
-from config import API_KEY_PEXELS, IMAGES_FOLDER, CSV_COLUMNS
+from config import API_KEY_PEXELS, IMAGES_FOLDER, CSV_COLUMNS, TEXT_CSV_FILE
 import spacy
-
+import pandas as pd
 
 # Load the English language model
 nlp = spacy.load("en_core_web_sm")
@@ -12,11 +12,45 @@ textIndex = CSV_COLUMNS.index('text')
 productLinkIndex = CSV_COLUMNS.index('prododuct_link')
 audioNameIndex = CSV_COLUMNS.index('audio_name')
 statusIndex = CSV_COLUMNS.index('status')
+    
 
 def getSentences(text):
     doc = nlp(text)
-    sentences = [sent.string.strip() for sent in doc.sents]
+    sentences = [sent for sent in doc.sents]
     return sentences
+
+def getPartsOfSpeech(sentence):
+    doc = nlp(sentence)
+    # Get the parts of speech
+    # token.pos_ is POS tag and token.text is the word
+    return [token for token in doc]
+
+def getNamedEntityRelationships(sentence):
+    doc = nlp(sentence)
+    ner = [(ent.text, ent.start_char, ent.end_char, ent.label_) for ent in doc.ents]
+    return ner
+
+def getWordsForImageSearch(text):
+    sentences = getSentences(text)
+    print(sentences)
+    for sentence in sentences:
+        words = getPartsOfSpeech(sentence.doc)
+        entities = getNamedEntityRelationships(sentence)
+        print(words)
+        print(entities)
+
+
+def imagesBasedOnSearchedWords():
+    df = pd.read_csv(TEXT_CSV_FILE, names=CSV_COLUMNS, skiprows=[0])
+
+    for index, row in df.iterrows():
+        # print(row.tolist())
+        if(df['status'][index] == 'complete' ):
+            continue
+        getWordsForImageSearch(row[textIndex])
+        df['status'][index] = 'complete'
+    df.to_csv(TEXT_CSV_FILE, index=False, header=False)
+
 
 
 def downloadAndSaveImage(main_word, category= 'technology'):
@@ -45,22 +79,6 @@ def saveImageIntoFile(imgUrl):
 
 
 if __name__ == "__main__":
-    downloadAndSaveImage('tablets', 'technology')
+    # downloadAndSaveImage('tablets', 'technology')
+    imagesBasedOnSearchedWords()
 
-
-
-# API()
-
-# df = pd.read_csv(TEXT_CSV_FILE, names=COLUMNS, skiprows=[0])
-# print(df)
-# for index, row in df.iterrows():
-#     # print(row.tolist())
-#     if(df['status'][index] == 'complete' ):
-#         continue
-#     t1 = gtts.gTTS(text=row[textIndex], lang='en', slow=False)
-#     audio_file = row[audioNameIndex]+ '.wav'
-#     audio_file_path = AUTDIOS_FOLDER + audio_file
-#     t1.save(audio_file)
-#     os.rename(audio_file, audio_file_path)
-#     df['status'][index] = 'complete'
-# df.to_csv(TEXT_CSV_FILE, index=False, header=False)
